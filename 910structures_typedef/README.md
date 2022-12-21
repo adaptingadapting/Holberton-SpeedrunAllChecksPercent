1647e78c546da559f53b2832f99a916d6c2e4a46
adaptingadapting <elpatoninjasuperninja@gmail.com>
2022-12-21 18:33:48 -0300

    task 0

---
+++ 910structures_typedef/README.md
@@ -0,0 +1,3 @@
+README for the structures typedef dir
+
+task 0 is just defining a structure in a header, shouldnt be too hard. it was the first time i did it though, thats good that ive improved so much
---
+++ 910structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef dog_h
+#define dog_h
+
+/**
+ * struct dog - dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+
+typedef struct dog = {
+  char *name;
+  float age;
+  char *owner;
+}dog;
+
+#endif dog_h
