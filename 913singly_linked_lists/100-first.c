4f60349c5cf1c361d48eb8dbb1ff71221309b73c
adaptingadapting <elpatoninjasuperninja@gmail.com>
2022-12-23 17:48:07 -0300

    task 100

---
+++ 913singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+void printa(void) __attribute__ ((constructor));
+
+/**
+ * printa - uses an attribute to print in compilation
+ */
+
+void printa(void)
+{
+	printf("You're beat! and yet, you must allow\nIbore my house upoon my back\n");
+}
