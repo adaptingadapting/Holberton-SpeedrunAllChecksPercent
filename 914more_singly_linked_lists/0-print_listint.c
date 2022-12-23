daf830c59c74d2f7de449d238efb61b86387395c
adaptingadapting <elpatoninjasuperninja@gmail.com>
2022-12-23 19:10:43 -0300

    task 0

---
+++ 914more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints a list
+ * @h: head
+ * Return: number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	int i = 0;
+
+	for (; h; h = h->next)
+	{
+		printf("%d\n", h->n);
+		i++;
+	}
+	return (i);
+}
--- 914more_singly_linked_lists/README.md
+++ 914more_singly_linked_lists/README.md
@@ -1,1 +1,3 @@
 README for the more s linked lists project
+
+task 0 is a simpler version of the first excercise of the last one, playing with the  head pointer gives me a little bit of a bad taste still but ok, i dont see any other way to do it
