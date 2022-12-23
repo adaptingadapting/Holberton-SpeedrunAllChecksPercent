bdbf06bcc28aa9121714d69171616637848ccee8
adaptingadapting <elpatoninjasuperninja@gmail.com>
2022-12-22 21:27:20 -0300

    changed the accepted format from string to char

--- 001_BIG_PROJECT_NUMBER_1:PRINTF/get_function.c
+++ 001_BIG_PROJECT_NUMBER_1:PRINTF/get_function.c
@@ -6,19 +6,19 @@
  * Return: function
  */
 
-int (*get_function(char *string))(va_list ap)
+int (*get_function(char format))(va_list ap)
 {
 	fmt_funcs functions[] = {
-		{"c", _pchar},
-		{"s", _pstring},
-		{"%", _ppercentage},
-		{NULL, NULL}
+		{'c', _pchar},
+		{'s', _pstring},
+		{'%', _ppercentage},
+		{'\0', NULL}
 	};
 	int i;
 
 	for (i = 0; functions[i].format; i++)
 	{
-		if (!strcmp(functions[i].format, string))
+		if (functions[i].format == format)
 			return (functions[i].f);
 	}
 	return (NULL);
---
+++ 001_BIG_PROJECT_NUMBER_1:PRINTF/main.h
@@ -0,0 +1,26 @@
+#ifndef PRINTF_H
+#define PRINTF_H
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+
+/**
+ * struct fmt_functions - stores functions
+ * @format: formats to compare
+ * @f: function to execute format
+ */
+
+typedef struct fmt_functions {
+	char format;
+	int (*f)(va_list ap);
+}fmt_funcs;
+
+int (*get_function(char c))(va_list ap);
+int _pstring(va_list ap);
+int _pchar(va_list ap);
+int _ppercentage(va_list ap __attribute__ ((unused)));
+
+#endif
