cc13aecd79409018c9b39c51d782f076e2f46dfe
adaptingadapting <elpatoninjasuperninja@gmail.com>
2022-12-28 20:51:20 -0300

    task 2

--- 915bit_manipulation/main.h
+++ 915bit_manipulation/main.h
@@ -6,6 +6,7 @@
 #include <stddef.h>
 #include <unistd.h>
 
+int get_bit(unsigned long int n, unsigned int index);
 unsigned int binary_to_uint(const char *b);
 unsigned int elevator(int n, int base, int elevator);
 void print_binary(unsigned long int n);
